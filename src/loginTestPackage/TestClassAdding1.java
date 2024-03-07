244195df9bc67f3b17f754017aaeeffe65b7cc34
Babarao <raometkar@gmail.com>
2024-03-08 00:29:43 +0530

    Task1Adding

---
+++ src/loginTestPackage/TestClassAdding1.java
@@ -0,0 +1,8 @@
+package loginTestPackage;
+
+public class TestClassAdding1 {
+
+	public void test123() {
+		System.out.println("Added class 1");
+	}
+}
---
+++ src/loginTestPackage/TestClassAdding2.java
@@ -0,0 +1,7 @@
+package loginTestPackage;
+
+public class TestClassAdding2 {
+	public void test456() {
+		System.out.println("Added class 2");
+	}
+}
