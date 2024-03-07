37ed995ca32bd01ddbb389596a3928ba51c3dc82
Babarao <raometkar@gmail.com>
2024-03-08 01:32:01 +0530

    done the changes

--- src/loginTestPackage/LoginTestClass.java
+++ src/loginTestPackage/LoginTestClass.java
@@ -5,7 +5,7 @@ public class LoginTestClass {
 	public void loginClassMethod() {
 		
 		System.out.println("Enter valid credentials");
-		System.out.println("Enter username and Password by user2 branch");
-		System.out.println("I am on new branch");
+		System.out.println("Enter username and Password by abc user2 branch");
+		System.out.println("I am abc on new branch");
 	}
 }
