985b709225534a360a33fd18eb65baaa7d86c1d1
Babarao <raometkar@gmail.com>
2024-03-08 00:34:27 +0530

    Task 2 changed the code

--- src/loginTestPackage/LoginTestClass.java
+++ src/loginTestPackage/LoginTestClass.java
@@ -1,5 +1,11 @@
 package loginTestPackage;
 
 public class LoginTestClass {
-
+	
+	public void loginClassMethod() {
+		
+		System.out.println("Enter valid credentials");
+		System.out.println("Enter username");
+		System.out.println("Enter password");
+	}
 }
