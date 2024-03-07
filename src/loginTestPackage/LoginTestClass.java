38b7a0fe36086f0cdab8b0050639545bf0ee1f12
Babarao <raometkar@gmail.com>
2024-03-08 01:14:07 +0530

    Changes done by user2

--- src/loginTestPackage/LoginTestClass.java
+++ src/loginTestPackage/LoginTestClass.java
@@ -5,7 +5,7 @@ public class LoginTestClass {
 	public void loginClassMethod() {
 		
 		System.out.println("Enter valid credentials");
-		System.out.println("Enter username");
-		System.out.println("Enter password");
+		System.out.println("Enter username and Password by user2 branch");
+		System.out.println("I am on new branch");
 	}
 }
